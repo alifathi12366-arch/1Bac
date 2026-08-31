91f8650876f6c9cb13f2cbd72a8318d6627ffff0
alifathi12366-arch <alifathi12366@gmail.com>
2026-08-31 20:10:55 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/bac1/MainActivity.kt
+++ app/src/main/java/com/example/bac1/MainActivity.kt
@@ -103,6 +103,7 @@ class MainActivity : AppCompatActivity() {
         val webView = findViewById<android.webkit.WebView>(R.id.adWebView)
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
+        webView.settings.userAgentString = "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
         webView.settings.mixedContentMode = android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
         webView.webViewClient = android.webkit.WebViewClient()
         android.webkit.WebView.setWebContentsDebuggingEnabled(true)
