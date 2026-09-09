6eee82905df3618811fd14d681c3eb7ebbb77bad
alifathi12366-arch <alifathi12366@gmail.com>
2026-09-09 19:45:48 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,9 @@ android {
         applicationId = "com.example.bac1"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+versionName = "2.0"
+
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
