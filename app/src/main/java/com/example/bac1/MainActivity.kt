da227066a4166a80a079d717d186014d3ca0dfa4
alifathi12366-arch <alifathi12366@gmail.com>
2026-09-17 23:18:33 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/bac1/MainActivity.kt
+++ app/src/main/java/com/example/bac1/MainActivity.kt
@@ -15,14 +15,13 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val prefs = getSharedPreferences("AppPrefs", MODE_PRIVATE)
-if (!prefs.getBoolean("is_registered", false)) {
-    startActivity(Intent(this, RegisterChoiceActivity::class.java))
-    finish()
-    return
-}
 
         val prefs = getSharedPreferences("bac1_prefs", MODE_PRIVATE)
+        if (!prefs.getBoolean("is_registered", false)) {
+            startActivity(Intent(this, RegisterChoiceActivity::class.java))
+            finish()
+            return
+        }
         if (prefs.getBoolean("is_teacher", false)) {
             startActivity(Intent(this, TeacherDashboardActivity::class.java))
             finish()
