33a7b3201a64f78b916b3941a84a5137305c858c
alifathi12366-arch <alifathi12366@gmail.com>
2026-09-18 12:15:16 +0300

    Update TeacherDashboardActivity.kt

--- app/src/main/java/com/example/bac1/TeacherDashboardActivity.kt
+++ app/src/main/java/com/example/bac1/TeacherDashboardActivity.kt
@@ -83,6 +83,9 @@ class TeacherDashboardActivity : AppCompatActivity() {
         findViewById<Button>(R.id.setJoinCodeButton).setOnClickListener {
             startActivity(Intent(this, SetJoinCodeActivity::class.java))
         }
+        findViewById<Button>(R.id.btnViewResults).setOnClickListener {
+            startActivity(Intent(this, TeacherResultsActivity::class.java))
+        }
 
         btnSelectPdf.setOnClickListener {
             selectPdfLauncher.launch("application/pdf")
