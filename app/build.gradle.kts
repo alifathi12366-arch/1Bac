8037c1298459842ff58f42549a268ea511881d5e
alifathi12366-arch <alifathi12366@gmail.com>
2026-09-22 22:05:51 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,5 +54,6 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
 implementation("com.google.firebase:firebase-analytics")
 implementation("com.google.firebase:firebase-storage-ktx")
+implementation("com.github.bumptech.glide:glide:4.16.0")
 
 }
